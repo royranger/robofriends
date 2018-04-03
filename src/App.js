e3e7ae05212f21e592222d8245489fc82b35cc79
Joy Goh-Mah <royran9er@gmail.com>
2018-04-04 00:44:16 +0100

    adds Searchbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 
 class App extends React.Component {
 
   render () {
     return(
-      <CardList/>
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox/>
+        <CardList/>
+      </div>
+
     );
   }
 
---
+++ src/SearchBox.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class SearchBox extends React.Component {
+
+  render() {
+    return(
+      <div className='pa2'>
+        <input
+          className='pa3 ba b--green bg-lightest-blue'
+          type='search'
+          placeholder='search robots'
+        />
+      </div>
+
+    );
+  }
+}
+
+export default SearchBox;
