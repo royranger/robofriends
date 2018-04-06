8cb657ff9e2691ad381638f8b179a3994169c052
Joy Goh-Mah <royran9er@gmail.com>
2018-04-06 20:39:04 +0100

    makes scroll component height dependent on viewport

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,8 +3,10 @@ import React, {Component} from 'react';
 class Scroll extends Component {
 
   render() {
+    const height = (window.innerHeight - 200);
+
     return(
-      <div style={{overflowY: 'scroll', border: '1px solid black', height: '400px'}}>
+      <div style={{overflowY: 'scroll', border: '1px solid black', height: `${height}px`}}>
         {this.props.children}
       </div>
     );
